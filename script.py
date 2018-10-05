625ea168eb5f81e8dbe70018f12dbf58c44aa61a
tapumar@gmail.com <tapumar@gmail.com>
2018-10-05 16:51:51 -0100

    some changes

--- script.py
+++ script.py
@@ -5,36 +5,43 @@ from urllib import request
 from progress.bar import Bar
 from datetime import datetime
 
-while(True):
-    logger = {'erros':[]}
-    error = []
-    filename = "lista_urls.txt"
-    nomeJson = str(datetime.now()) +".json"
-    with open(filename) as casas:
-        casas = list(casas)
-        bar = Bar('Loading', fill='@', suffix='%(percent)d%%',max= 1000)
-        casas = casas[1:]
+filename = "lista_urls.txt"
 
-    def execute_url_list(casa):
-        try:
-            start = time.time()
-            result = request.urlopen(casa)
-            end = time.time()
-            if result.status != 200:
-                log = {'url':casa,'status':result.status,'tempo':end-start}
-                logger['erros'].append(log)
-        except Exception as e:
-            end = time.time()
-            if e:
-                log = {'url':casa, 'status':str(e),'tempo':end-start}
-                logger['erros'].append(log)
+def create_json(nomeJson, logger):
+    with open(nomeJson, 'w') as fp:
+        json.dump(logger, fp)
+def execute_url_list(casa, logger):
+    try:
+        start = time.time()
+        result = request.urlopen(casa)
+        end = time.time()
+        if result.status != 200:
+            log = {'url':casa,'status':result.status,'tempo':end-start}
+            logger['erros'].append(log)
+    except Exception as e:
+        end = time.time()
+        if e:
+            log = {'url':casa, 'status':str(e),'tempo':end-start}
+            logger['erros'].append(log)
+    return (logger)
+    create_json(nomeJson, logger)
 
-        with open(nomeJson, 'w') as fp:
-            json.dump(logger, fp)
 
-    threads = [threading.Thread(target=execute_url_list, args=(casa,)) for casa in casas]
 
-    #Rotina da Thread
+# while(True):
+#     logger = {'erros':[]}
+#     filename = "lista_urls.txt"
+#     nomeJson = str(datetime.now()) +".json"
+#     with open(filename) as casas:
+#         casas = list(casas)
+#         bar = Bar('Loading', fill='@', suffix='%(percent)d%%',max= 1000)
+#         casas = casas[1:]
+
+
+def threads(casas, logger, nomeJson):
+    bar = Bar('Loading', fill='@', suffix='%(percent)d%%',max= 1000)
+    threads = [threading.Thread(target=execute_url_list,
+              args=(casa,logger, nomeJson, )) for casa in casas]
     print("Start")
     for thread in threads:
         thread.start()
@@ -42,6 +49,21 @@ while(True):
     for thread in threads:
         thread.join()
     bar.goto(1000)
-    print()
-    print("Done!")
-    time.sleep(300)
+
+
+def main():
+    while(True):
+        logger = {'erros':[]}
+        nomeJson = str(datetime.now()) +".json"
+        with open(filename) as casas:
+            casas = list(casas)
+            casas = casas[1:]
+        threads(casas, logger, nomeJson)
+        print()
+        print("Done!")
+        time.sleep(300)
+
+
+
+if __name__ == '__main__':
+    main()
